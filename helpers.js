d5e3d53702627ffdbb1acdde31be0555a35ebd91
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-11-13 19:36:03 +0000

    Refactor Helper Functions

--- helpers.js
+++ helpers.js
@@ -1,3 +1,21 @@
+const urlDatabase = { //it's an object
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },  //urlDatabase[req.params.shortURL]["longURL"]
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
+};
+
+const users = {     //Create a users Object   access id : users.id
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 //helper function :   db: user database
 const fetchEmail = (db, email) => {
   for(const id in db) { //every key:id
@@ -8,4 +26,25 @@ const fetchEmail = (db, email) => {
   return false;
 };
 
-module.exports = { fetchEmail }
+
+const urlsForUser = function(id) {
+  const urlsOfUser = {};
+  for (const key in urlDatabase) {
+    if(urlDatabase[key]["userID"] === id) {
+      urlsOfUser[key] = urlDatabase[key];
+    }
+  }
+  return urlsOfUser;
+}
+
+// const updateUrlDatabase = function(shortURL, longURL, userID) {
+
+//   //add 
+
+//   urlDatabase[shortURL] = {
+//     longURL,
+//     userID
+//   }
+// }
+
+module.exports = { fetchEmail , urlsForUser, urlDatabase, users }
